0fa85d5eaeaf94921c5448e8ddbaeb8cf519409b
ZubaerJewel <zubaer5jewel.16@gmail.com>
2023-08-30 17:12:38 +0600

    store previous conversation on a specific room

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,10 +1,93 @@
 
+// import React, { useEffect, useState } from "react";
+// import ScrollToBottom from "react-scroll-to-bottom";
+
+// function Chat({ socket, username, room, notification, setNotification }) {
+//   const [currentMessage, setCurrentMessage] = useState("");
+//   const [messageList, setMessageList] = useState([]); // Declare setMessageList here
+
+//   const sendMessage = async () => {
+//     if (currentMessage !== "") {
+//       const messageData = {
+//         room: room,
+//         author: username,
+//         message: currentMessage,
+//         time:
+//           new Date(Date.now()).getHours() +
+//           ":" +
+//           new Date(Date.now()).getMinutes(),
+//       };
+
+//       await socket.emit("send_message", messageData);
+//       setMessageList((list) => [...list, messageData]);
+//       setCurrentMessage("");
+//     }
+//   };
+
+//   useEffect(() => {
+//     socket.on("receive_message", (data) => {
+//       setMessageList((list) => [...list, data]);
+      
+//       if (data.author !== username) {
+//         setNotification(true);
+//       }
+//     });
+//   }, [socket, username]);
+
+//   return (
+//         <div className="chat-window">
+//           <div className="chat-header">
+//             <p>Live Chat</p>
+//           </div>
+//           <div className="chat-body">
+//             <ScrollToBottom className="message-container">
+//               {messageList.map((messageContent) => {
+//                 return (
+//                   <div
+//                     className="message"
+//                     id={username === messageContent.author ? "you" : "other"}
+//                   >
+//                     <div>
+//                       <div className="message-content">
+//                         <p>{messageContent.message}</p>
+//                       </div>
+//                       <div className="message-meta">
+//                         <p id="time">{messageContent.time}</p>
+//                         <p id="author">{messageContent.author}</p>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 );
+//               })}
+//             </ScrollToBottom>
+//           </div>
+//           <div className="chat-footer">
+//             <input
+//               type="text"
+//               value={currentMessage}
+//               placeholder="Hey..."
+//               onChange={(event) => {
+//                 setCurrentMessage(event.target.value);
+//               }}
+//               onKeyPress={(event) => {
+//                 event.key === "Enter" && sendMessage();
+//               }}
+//             />
+//             <button onClick={sendMessage}>&#9658;</button>
+//           </div>
+//         </div>
+//       );
+// }
+
+// export default Chat;
+// Chat.js
 import React, { useEffect, useState } from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
 
 function Chat({ socket, username, room, notification, setNotification }) {
   const [currentMessage, setCurrentMessage] = useState("");
-  const [messageList, setMessageList] = useState([]); // Declare setMessageList here
+  const [messageList, setMessageList] = useState([]);
+  const [conversation, setConversation] = useState([]); // New state for conversation history
 
   const sendMessage = async () => {
     if (currentMessage !== "") {
@@ -17,66 +100,84 @@ function Chat({ socket, username, room, notification, setNotification }) {
           ":" +
           new Date(Date.now()).getMinutes(),
       };
-
+     
       await socket.emit("send_message", messageData);
       setMessageList((list) => [...list, messageData]);
       setCurrentMessage("");
+      socket.emit("get_conversation", room);
     }
+  };
+
+  const loadConversation = () => {
+    socket.emit("get_conversation", room); // Request previous conversation
   };
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
       setMessageList((list) => [...list, data]);
-      
+
       if (data.author !== username) {
         setNotification(true);
       }
     });
+
+    // Listen for previous_conversation event from server
+    socket.on("previous_conversation", (data) => {
+      setConversation(data);
+    });
+
+    // Clean up listeners on component unmount
+    return () => {
+      socket.off("receive_message");
+      socket.off("previous_conversation");
+    };
   }, [socket, username]);
 
   return (
-        <div className="chat-window">
-          <div className="chat-header">
-            <p>Live Chat</p>
-          </div>
-          <div className="chat-body">
-            <ScrollToBottom className="message-container">
-              {messageList.map((messageContent) => {
-                return (
-                  <div
-                    className="message"
-                    id={username === messageContent.author ? "you" : "other"}
-                  >
-                    <div>
-                      <div className="message-content">
-                        <p>{messageContent.message}</p>
-                      </div>
-                      <div className="message-meta">
-                        <p id="time">{messageContent.time}</p>
-                        <p id="author">{messageContent.author}</p>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            </ScrollToBottom>
-          </div>
-          <div className="chat-footer">
-            <input
-              type="text"
-              value={currentMessage}
-              placeholder="Hey..."
-              onChange={(event) => {
-                setCurrentMessage(event.target.value);
-              }}
-              onKeyPress={(event) => {
-                event.key === "Enter" && sendMessage();
-              }}
-            />
-            <button onClick={sendMessage}>&#9658;</button>
-          </div>
-        </div>
-      );
+    <div className="chat-window">
+      <div className="chat-header">
+        <p>Live Chat</p>
+        {notification && <span>New Message!</span>}
+      </div>
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {/* Render the conversation history */}
+          {conversation.map((messageContent, index) => (
+            <div
+              className="message"
+              id={username === messageContent.author ? "you" : "other"}
+              key={index}
+            >
+              <div>
+                <div className="message-content">
+                  <p>{messageContent.message}</p>
+                </div>
+                <div className="message-meta">
+                  <p id="time">{messageContent.time}</p>
+                  <p id="author">{messageContent.author}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </ScrollToBottom>
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          value={currentMessage}
+          placeholder="Hey..."
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
+          }}
+        />
+        <button onClick={sendMessage}>&#9658;</button>
+        {/* <button onClick={loadConversation}>Load Previous Conversation</button> */}
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -15,8 +14,7 @@ const io = new Server(server, {
   },
 });
 
-// Create a map to store socket IDs and usernames
-const users = new Map();
+const conversationMap = new Map(); // Map to store conversation history by room
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
@@ -24,25 +22,82 @@ io.on("connection", (socket) => {
   socket.on("join_room", (data) => {
     socket.join(data);
     console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    users.set(socket.id, data); // Store socket ID and room mapping
+
+    // Send previous conversation to the user when they join a room
+    const roomConversation = conversationMap.get(data) || [];
+    socket.emit("previous_conversation", roomConversation);
+  });
+
+  socket.on("get_conversation", (room) => {
+    const roomConversation = conversationMap.get(room) || [];
+    socket.emit("previous_conversation", roomConversation);
   });
 
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data);
 
-    // Notify the sender when the message is received by the recipient
-    const senderRoom = users.get(socket.id);
-    if (senderRoom !== data.room) {
-      socket.emit("message_received", { recipient: data.author });
+    // Add the message to the conversationMap
+    if (!conversationMap.has(data.room)) {
+      conversationMap.set(data.room, []);
     }
+    conversationMap.get(data.room).push(data);
   });
 
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
-    users.delete(socket.id); // Remove user from the map
   });
 });
 
 server.listen(3001, () => {
   console.log("SERVER RUNNING");
 });
+
+
+// const express = require("express");
+// const app = express();
+// const http = require("http");
+// const cors = require("cors");
+// const { Server } = require("socket.io");
+// app.use(cors());
+
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// // Create a map to store socket IDs and usernames
+// const users = new Map();
+
+// io.on("connection", (socket) => {
+//   console.log(`User Connected: ${socket.id}`);
+
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
+//     users.set(socket.id, data); // Store socket ID and room mapping
+//   });
+
+//   socket.on("send_message", (data) => {
+//     socket.to(data.room).emit("receive_message", data);
+
+//     // Notify the sender when the message is received by the recipient
+//     const senderRoom = users.get(socket.id);
+//     if (senderRoom !== data.room) {
+//       socket.emit("message_received", { recipient: data.author });
+//     }
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("User Disconnected", socket.id);
+//     users.delete(socket.id); // Remove user from the map
+//   });
+// });
+
+// server.listen(3001, () => {
+//   console.log("SERVER RUNNING");
+// });
+
